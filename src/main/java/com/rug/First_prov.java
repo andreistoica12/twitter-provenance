942d8e8b37c0b2c123a8fb85fde0fe4727007c3d
andreistoica12 <andreistoica12@gmail.com>
2023-07-06 15:12:58 +0200

    preliminary Maven tests - Hello World!

---
+++ src/main/java/com/rug/First_prov.java
@@ -0,0 +1,26 @@
+package com.rug;
+
+import java.util.ArrayList;
+
+import org.openprovenance.prov.configuration.Configuration;
+import org.openprovenance.prov.interop.Formats;
+import org.openprovenance.prov.interop.InteropFramework;
+import org.openprovenance.prov.model.Agent;
+import org.openprovenance.prov.model.Document;
+import org.openprovenance.prov.model.Entity;
+import org.openprovenance.prov.model.Namespace;
+import org.openprovenance.prov.model.QualifiedName;
+import org.openprovenance.prov.model.ProvFactory;
+import org.openprovenance.prov.model.StatementOrBundle;
+import org.openprovenance.prov.model.WasAttributedTo;
+import org.openprovenance.prov.model.WasDerivedFrom;
+
+
+public class First_prov 
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
