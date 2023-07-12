f4877f1b9bd6ffe7f64008635c3506ba071d393d
andreistoica12 <andreistoica12@gmail.com>
2023-07-12 19:06:28 +0200

    started template implementation for the actual Twitter model

--- src/main/java/com/rug/Template.java
+++ src/main/java/com/rug/Template.java
@@ -4,6 +4,13 @@ package com.rug;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.GregorianCalendar;
+import java.util.Date;
+
+
+import javax.xml.datatype.DatatypeConstants;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.openprovenance.prov.interop.Formats;
 import org.openprovenance.prov.interop.InteropFramework;
@@ -75,43 +82,76 @@ public class Template {
         
     public Document createTemplate() {
 
-        // Entity declaration with attributes
+        // 1. ENTITY - originalTweet - declaration with attributes
         Collection<Attribute> tweetAttributes = new ArrayList<>();
         Attribute tweetValue = pFactory.newAttribute(Attribute.AttributeKind.PROV_VALUE, qn_var("text"), pFactory.getName().XSD_ANY_URI);
         tweetAttributes.add(tweetValue);
-        Entity tweet = pFactory.newEntity(qn_var("tweet"), tweetAttributes);
+        Entity entity_originalTweet = pFactory.newEntity(qn_var("original_post_id"), tweetAttributes);
 
 
-        // Agent declaration with attributes
-        Collection<Attribute> agentAttributes = new ArrayList<>();
-        Attribute type = pFactory.newAttribute(Attribute.AttributeKind.PROV_TYPE, qn_prov("Person"), pFactory.getName().XSD_STRING);
-        Attribute name = pFactory.newAttribute(qn_foaf("name"), qn_var("name"), pFactory.getName().XSD_STRING);
-        agentAttributes.add(type);
-        agentAttributes.add(name);
-        Agent author = pFactory.newAgent(qn_var("authorId"), agentAttributes);
+
+        // 2. ACTIVITY - post - declaration with attributes
+        Collection<Attribute> postActivityAttributes = new ArrayList<>();
+        Attribute postType = pFactory.newAttribute(Attribute.AttributeKind.PROV_TYPE, "publish", pFactory.getName().XSD_STRING);
+        postActivityAttributes.add(postType);
+
+        // As I want to add the type attribute to the post activity, the constructor requires to define 2
+        // XMLGregorianCalendar values for start and end date.
+        // Create Date Object
+        Date current_date = new Date();
+        XMLGregorianCalendar xmlStartDate = null;
+        XMLGregorianCalendar xmlEndDate = null;
+        // Gregorian Calendar object creation
+        GregorianCalendar gc = new GregorianCalendar();
+        // giving current date and time to gc
+        gc.setTime(current_date);
+        
+        try {
+            xmlStartDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(gc);
+
+            // Add 2 days to the current timestamp
+            gc.add(GregorianCalendar.DAY_OF_YEAR, 2);
+
+            xmlEndDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(gc);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Activity activity_post = pFactory.newActivity(qn_var("post_id"), xmlStartDate, xmlEndDate, postActivityAttributes);
+        
 
 
-        // Attribution declaration with attributes
-        WasAttributedTo attr1 = pFactory.newWasAttributedTo(null, tweet.getId(), author.getId());
+        // 3. GENERATION - wgb1 - declaration with attributes
+        WasGeneratedBy wgb1 = pFactory.newWasGeneratedBy(null, entity_originalTweet.getId(), activity_post.getId());
+
+
+        // 4. AGENT - originalAuthor - declaration with attributes
+        Collection<Attribute> originalAuthorAttributes = new ArrayList<>();
+        Attribute originalAuthorType = pFactory.newAttribute(Attribute.AttributeKind.PROV_TYPE, qn_prov("Person"), pFactory.getName().XSD_STRING);
+        Attribute originalAuthorName = pFactory.newAttribute(qn_foaf("name"), qn_var("name"), pFactory.getName().XSD_STRING);
+        originalAuthorAttributes.add(originalAuthorType);
+        originalAuthorAttributes.add(originalAuthorName);
+        Agent agent_originalAuthor = pFactory.newAgent(qn_var("original_author_id"), originalAuthorAttributes);
+
+
+        // 5. ASSOCIATION - assoc1 - declaration with attributes
+        Collection<Attribute> assoc1Attributes = new ArrayList<>();
+        Attribute assoc1Role = pFactory.newAttribute(Attribute.AttributeKind.PROV_ROLE, "author", pFactory.getName().XSD_STRING);
+        assoc1Attributes.add(assoc1Role);
+        WasAssociatedWith assoc1 = pFactory.newWasAssociatedWith(qn_var("assoc1_id"), activity_post.getId(), agent_originalAuthor.getId(), (QualifiedName)null, assoc1Attributes);
 
 
 
         // Create a collection to store statements
         Collection<Statement> statementCollection = new ArrayList<>();
-        statementCollection.add(tweet);
-        statementCollection.add(author);
-        statementCollection.add(attr1);
+        statementCollection.addAll(Arrays.asList(entity_originalTweet, activity_post, agent_originalAuthor, wgb1, assoc1));
+
         Bundle bundle = pFactory.newNamedBundle(qn_vargen("bundleId"), ns, statementCollection);
 
 
         Document document = pFactory.newDocument();
         document.getStatementOrBundle().add(bundle);
-
-
-        // Document document = pFactory.newDocument();
-        // document.getStatementOrBundle().addAll(Arrays.asList(tweet, author, attr1));
-        // document.setNamespace(ns);
-
 
         return document;
     }
